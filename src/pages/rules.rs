5f9fe3f64c4f2e1bca55356d5ecc704b03260fa2
Stephen M. Coakley <me@stephencoakley.com>
2024-07-28 22:28:15 -0500

    Use slice for render_rule, add whitespace config for rs files

--- src/pages/rules.rs
+++ src/pages/rules.rs
@@ -1,4 +1,4 @@
-use crate::{components::layout::layout, 
+use crate::{components::layout::layout,
 domain::rules::{get_rulebook, Rule, RuleChild, List, ListItem, ListOption, ContentData}};
 
 use maud::{html, Markup};
@@ -29,126 +29,126 @@ where
 //	//ids.join(".").into();
 //}
 
-fn render_rule(rule: &Rule, ids: &Vec<u16>) -> Markup {
-	let mut my_ids: Vec<u16> = ids.clone();
-	my_ids.push(rule.id);
-	let id_string: String = my_ids.clone().join(".");
+fn render_rule(rule: &Rule, ids: &[u16]) -> Markup {
+    let mut my_ids: Vec<u16> = ids.to_vec();
+    my_ids.push(rule.id);
+    let id_string: String = my_ids.clone().join(".");
 
-	html! {	
-		@if let Some(title) = &rule.title {
-			@if ids.len() == 0 {
-				div.rule_title { (id_string) }
-				div.rule_title { (title) }
-				hr;
-			} @else {
-				br;
-				div.rule_subtitle { (id_string) }
-				div.rule_subtitle { (title) }
-			}
-		} @else {
-			//only print the rule
-			@if let Some(t) = &rule.r#type {
-				@if !t.eq_ignore_ascii_case("hidden") {
-					div.rule_normal { (id_string) }
-				}
-			} @else {
-				div.rule_normal { (id_string) }
-			}
-		}
+    html! {
+        @if let Some(title) = &rule.title {
+            @if ids.is_empty() {
+                div.rule_title { (id_string) }
+                div.rule_title { (title) }
+                hr;
+            } @else {
+                br;
+                div.rule_subtitle { (id_string) }
+                div.rule_subtitle { (title) }
+            }
+        } @else {
+            //only print the rule
+            @if let Some(t) = &rule.r#type {
+                @if !t.eq_ignore_ascii_case("hidden") {
+                    div.rule_normal { (id_string) }
+                }
+            } @else {
+                div.rule_normal { (id_string) }
+            }
+        }
 
-		@for child in &rule.children {
-			@match child {
-				RuleChild::Rule(r) => {					
-					div { (render_rule(&r, &my_ids)) }
-				},
-				RuleChild::List(l) => {
-					div { (render_list(l)) }
-				},
-				RuleChild::Note(n) => {
-					p { (n.text) }
-				},
-				RuleChild::Section(s) => {
-					div.rule_normal { (s.text) }
-				}
-			}
-		}		
-	}
+        @for child in &rule.children {
+            @match child {
+                RuleChild::Rule(r) => {
+                    div { (render_rule(&r, &my_ids)) }
+                },
+                RuleChild::List(l) => {
+                    div { (render_list(l)) }
+                },
+                RuleChild::Note(n) => {
+                    p { (n.text) }
+                },
+                RuleChild::Section(s) => {
+                    div.rule_normal { (s.text) }
+                }
+            }
+        }
+    }
 }
 
 fn render_list(list: &List) -> Markup {
-	html! {	
-		// need to parse the list type
-		@if let Some(opt) = &list.option {
-			@match opt {
-				ListOption::Unordered => {
-					ul { (render_list_items(&list)) }
-				},
-				ListOption::Numeric => {
-					ol { (render_list_items(&list)) }
-				},
-				ListOption::AlphaLowercase => {
-					ol type="a" { (render_list_items(&list)) }
-				},
-				ListOption::AlphaUppercase => {
-					ol type="A" { (render_list_items(&list)) }
-				}
-			}
-		}
-	}
+    html! {
+        // need to parse the list type
+        @if let Some(opt) = &list.option {
+            @match opt {
+                ListOption::Unordered => {
+                    ul { (render_list_items(&list)) }
+                },
+                ListOption::Numeric => {
+                    ol { (render_list_items(&list)) }
+                },
+                ListOption::AlphaLowercase => {
+                    ol type="a" { (render_list_items(&list)) }
+                },
+                ListOption::AlphaUppercase => {
+                    ol type="A" { (render_list_items(&list)) }
+                }
+            }
+        }
+    }
 }
 
 fn render_list_items(list: &List) -> Markup {
-	html! {	
-		@for item in &list.items {
-			li { (item.text) }
-			@for section in &item.sections {
-				@match section {
-					ContentData::List(l) => {
-						div { (render_list(l)) }
-					},
-					ContentData::Note(n) => {
-						p { (n.text) }
-					},
-					ContentData::Section(s) => {
-						p { (s.text) }
-					}
-				}
-			}
-		}
-	}
+    html! {
+        @for item in &list.items {
+            li { (item.text) }
+            @for section in &item.sections {
+                @match section {
+                    ContentData::List(l) => {
+                        div { (render_list(l)) }
+                    },
+                    ContentData::Note(n) => {
+                        p { (n.text) }
+                    },
+                    ContentData::Section(s) => {
+                        p { (s.text) }
+                    }
+                }
+            }
+        }
+    }
 }
 
 pub fn render() -> Markup {
     let rulebook = get_rulebook();
-	let vec: Vec<u16> = Vec::new();
-	//dbg!(rulebook);
-	layout(
+    let vec: Vec<u16> = Vec::new();
+    //dbg!(rulebook);
+    layout(
         "Rules",
         html! {
             h1 { (rulebook.titlepage.title) }
- 
+
             h2 { (rulebook.titlepage.subtitle) }
             h3 { (rulebook.titlepage.edition) }
 
             p { (rulebook.titlepage.copyright) }
             br;
-			div { 
-				a href=(rulebook.titlepage.website.uri) { (rulebook.titlepage.website.text) } 
-				" | " 
-				a href=(rulebook.titlepage.email.uri) { (rulebook.titlepage.email.text) } 
-			}
+            div {
+                a href=(rulebook.titlepage.website.uri) { (rulebook.titlepage.website.text) }
+                " | "
+                a href=(rulebook.titlepage.email.uri) { (rulebook.titlepage.email.text) }
+            }
             br;
-			div { "Effective " (rulebook.titlepage.effective.format("%v")) }
+            div { "Effective " (rulebook.titlepage.effective.format("%v")) }
             br;
 
-			hr;
+            hr;
 
-			@for rule in &rulebook.contents.rules {
-				div { (render_rule(rule, &vec)) }
-			}
+            @for rule in &rulebook.contents.rules {
+                div { (render_rule(rule, &vec)) }
+            }
 
-			p;
-			p;
+            p;
+            p;
 
         },
     )
