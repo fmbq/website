9eece584dba52292b530cd32fc42cb04c445364f
Stephen M. Coakley <me@stephencoakley.com>
2025-03-20 22:18:57 -0500

    Use header constants

--- src/web/middleware/headers.rs
+++ src/web/middleware/headers.rs
@@ -1,14 +1,20 @@
-use poem::middleware::SetHeader;
+use poem::{
+    http::header::{CONTENT_SECURITY_POLICY, REFERRER_POLICY, X_CONTENT_TYPE_OPTIONS},
+    middleware::SetHeader,
+};
 
 /// Return a middleware that sets some static security headers.
 pub fn security_headers() -> SetHeader {
     SetHeader::new()
-        .appending("content-security-policy", "default-src 'self'; img-src 'self' placeholder.pics")
+        .appending(
+            CONTENT_SECURITY_POLICY,
+            "default-src 'self'; img-src 'self' placeholder.pics",
+        )
         .appending(
             "permissions-policy",
             "geolocation=(), microphone=(), camera=()",
         )
-        .appending("referrer-policy", "strict-origin-when-cross-origin")
-        .appending("x-content-type-options", "nosniff")
+        .appending(REFERRER_POLICY, "strict-origin-when-cross-origin")
+        .appending(X_CONTENT_TYPE_OPTIONS, "nosniff")
         .appending("x-frame-options", "DENY")
 }
